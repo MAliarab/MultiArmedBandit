3897977672046ebc11d839521b814f1b06dbcf3e
Mahmoud <arab6320@gmail.com>
2019-01-06 21:09:43 +0330

    last commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 //created by Mahmoud Aliarab
 //run and enjoy :))
+//code id available in https://github.com/MAliarab/MultiArmedBandit too ;)
 public class Main {
 
     public static void main(String[] args) {
